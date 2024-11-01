de30842d536866a7eb667a5863e573ca2fa57543
Igor Sadoune <131283914+IgorSadoune@users.noreply.github.com>
2024-11-01 15:44:43 -0400

    update metric verbose

--- src/modules/agents/d3qn_om_agent.py
+++ src/modules/agents/d3qn_om_agent.py
@@ -189,6 +189,6 @@ class D3QNOMAgent(OpponentModel):
 
     def get_metrics(self) -> Dict[str, float]:
         metrics_dict = {"loss": self.loss,
-                        "Q value defect": self.q_value_0,
-                        "Q value cooperate": self.q_value_1}
+                        "q-value defect": self.q_value_0,
+                        "q-value cooperate": self.q_value_1}
         return metrics_dict
